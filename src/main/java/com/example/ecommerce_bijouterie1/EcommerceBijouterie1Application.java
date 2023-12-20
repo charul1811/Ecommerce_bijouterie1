f75a8088e10f8995fb2fd63284c2a359597f00f4
cthacker <charulthacker@gmail.com>
2023-12-20 11:07:35 +0100

    Packages created

---
+++ src/main/java/com/example/ecommerce_bijouterie1/EcommerceBijouterie1Application.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_bijouterie1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBijouterie1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceBijouterie1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecommerce_bijouterie1/EcommerceBijouterie1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_bijouterie1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBijouterie1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
